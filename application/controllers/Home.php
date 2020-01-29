0f0c1d3bfc7f588aed213ff57fe2beae721d5018
Jaykhalid <59681918+Jaykhalid@users.noreply.github.com>
2020-01-29 11:47:14 +0700

    ekspor database

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,8 +20,6 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->helper('url');
-		$this->load->view('templates/header');
 		$this->load->view('home_page');
 	}
 }
