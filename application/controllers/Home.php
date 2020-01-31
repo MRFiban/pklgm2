7e05b082e64392e2f6eff2bce0ef5c40de479997
toniindriyanto12 <59683084+toniindriyanto12@users.noreply.github.com>
2020-01-31 10:04:19 +0700

    mengerjakan form login
    mengatur login dan register

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,6 +20,35 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('home_page');
+		$this->load->view('auth/login');
 	}
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function tampilan()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+    
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+
+        if( $this->form_validation->run() ==false){
+            $data['title'] = 'WPU User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        }else{
+            echo'data berhasil ditambahkan!';
+        }
+    }
 }
