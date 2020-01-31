2703db8152b8aeb7f1327438babe35d2c3e6865d
toniindriyanto12 <59683084+toniindriyanto12@users.noreply.github.com>
2020-01-31 15:33:13 +0700

    setting ulang controller form login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,34 +3,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+
 	public function index()
 	{
 		$this->load->view('website/home_page');
 	}
 
 	public function login()
-	{
-		$this->load->view('auth/login');
-	}
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
 
 		public function daftar()
-	{
-		$this->load->view('auth/registration');
-	}
+  {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+
+        if( $this->form_validation->run() ==false){
+            $data['title'] = 'WPU User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        }
+        
+        else
+        {
+            echo'data berhasil ditambahkan!';
+        }
+    }
 
 }
