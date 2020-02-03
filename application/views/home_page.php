6dcc751b845222f755db0705c193e1c586cba59f
MRFiban <57137344+MRFiban@users.noreply.github.com>
2020-02-03 15:20:12 +0700

    pklgm2
    geragawean

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -1,56 +1,36 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+		 <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7CPT+Serif:400,700,400italic' rel='stylesheet'>
+		  <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+</head>
 <body>
 
-	<div class="navbar1">
-		<a href="#default" id="logo">GLOBAL MEKAR MANDIRI 2</a>
-		<a href="#home">Home</a>
-		<a href="#news">Profil</a>
-		<a href="#news">Foto Direksi</a>
-		<div class="dropdown1">
-			<button class="dropbtn">Product
-				<i class="fa fa-caret-down"></i>
-			</button>
-			<div class="dropdown-content">
-				<div class="header">
-				</div>
-				<div class="row">
-					<div class="column1">
-						<h3>Product mechanical</h3>
-						<a href="#">Mesin IPAL</a>
-						<a href="#">Crusher</a>
-						<a href="#">Reaktor</a>
-						<a href="#">Filtering</a>
-						<a href="#">Konveyor</a>
-						<a href="#">Dice</a>
-						<a href="#">Shereder</a>
-						<a href="#">Membran</a>
-						<a href="#">Hydran rampang</a>
-					</div>
-					<div class="column1">
-						<h3>Electrical</h3>
-						<a href="#">Masining otomation controling</a>
-						<a href="#">Home Otomation</a>
-						<a href="#">Mechanical electrical</a>
-					</div>
-					<div class="column1">
-						<h3>Informasi Teknologi</h3>
-						<a href="#">LAN</a>
-						<a href="#">WAN</a>
-						<a href="#">Telemetri</a>
-						<a href="#">Software Development</a>
-						<a href="#">Web Development</a>
-					</div>
-				</div>
+	<div class="bgimage">
+		<div class="menu">
+			
+			<div class="leftmenu">
+				<h4> GELOBAL MEKAR MANDIRI </h4>
+			</div>
+
+			<div class="rightmenu">
+                <ul>
+					<li id="fisrtlist"> HOME </li>
+					<li> Frofil </li>
+					<li> Product</li>
+					<li>  Blogs </li>
+					<li>contact</li>
+                    </ul>
+                </div>
 			</div>
 		</div>
-	</div>
 
-	<div style="padding:16px">
-		<h3>Responsive Mega Menu (Full-width dropdown in navbar)</h3>
-		<p>Hover over the "Dropdown" link to see the mega menu.</p>
-		<p>Resize the browser window to see the responsive effect.</p>
+		<div class="text">
+			<h4> SOFTWARE DEVELOPMENT • WEB DEVELOPMENT </h4>
+			<h1> CREATIVE & EXPERIENCED </h1>
+			<h3> PT.GLOBAL MEKAR MANDIRI </h3>
+		</div>
 	</div>
-	<button class="btn btn-primary">HEHHE</button>
-
 </body>
-
 </html>
