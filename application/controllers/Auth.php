cfe14d1b535d2de64ec93621d24fa36764af6b46
MRFiban <ridwaniban24@gmail.com>
2020-02-14 08:00:32 +0700

    Revert "Merge branch 'WIP' of https://github.com/MRFiban/pklgm2 into WIP"
    This reverts commit 6ec9c7a25261a7fe9ed7902af3efd91883e6de98, reversing
    changes made to 4d91e370a4711f45bb3e9388b1b7016a9fcde2da.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,9 +13,13 @@ class Auth extends CI_Controller
 
     public function login()
     {
-        if ($this->form_validation->run == false) {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+
+        if ($this->form_validation->run() == false) {
             $data['title'] = 'Login Pages';
-            $this->load->view('templates/auth_header');
+            $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/login');
             $this->load->view('templates/auth_footer');
         }
@@ -24,38 +28,37 @@ class Auth extends CI_Controller
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
-        $this->form_validation->set_rules(
-            'password1',
-            'Password',
-            'required|trim|min_length[3]|matches[password2]',
-            [
-                'matches' => 'password is not match',
-                'min_length' => 'password too shorts'
-            ]
-        );
-        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
-
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'this email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[3]|matches[password1]');
 
         if ($this->form_validation->run() == false) {
-            $data['title'] = 'PT. GM2';
+            $data['title'] = 'WPU User Registration';
+
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
             $data = [
-
-                'name' => $this->input->post('name'),
-                'email' => $this->input->post('email'),
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
                 'image' => 'default.jpg',
-                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
-                'image' => 'default.jpg',
+                'password' => password_hash(
+                    $this->input->post('password1'),
+                    PASSWORD_DEFAULT
+                ),
                 'role_id' => 2,
                 'is_active' => 1,
                 'date_created' => time()
             ];
 
             $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Congratulation! your acccount has been created. Please Login </div>');
             redirect('auth/login');
         }
     }
