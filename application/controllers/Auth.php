bed7956468546978e4dc3843b48fdd6776f77627
MRFiban <ridwaniban24@gmail.com>
2020-02-14 10:31:08 +0700

    login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -35,10 +35,31 @@ class Auth extends CI_Controller
         $password = $this->input->post('password');
 
         $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+
+        //jika usernya ada
         if ($user) {
-            //usernya ada
+            //jika usernya aktif
+            if ($user['is_active'] == 1) {
+                //cek password
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('home');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Wrong password! </div>');
+                    redirect('auth/login');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> This email has not been activated ! </div>');
+                redirect('auth/login');
+            }
         } else {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Congratulation! your acccount has been created. Please Login </div>');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email is not registered! </div>');
             redirect('auth/login');
         }
     }
@@ -81,5 +102,15 @@ class Auth extends CI_Controller
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Congratulation! your acccount has been created. Please Login </div>');
             redirect('auth/login');
         }
+    }
+
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> You have been logout! </div>');
+        redirect('auth/login');
     }
 }
