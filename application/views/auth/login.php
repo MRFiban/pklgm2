4e1a26e5e0889628866af5530893330050aea69a
MRFiban <ridwaniban24@gmail.com>
2020-02-19 14:51:50 +0700

    AAA

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,5 +1,5 @@
 <!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-navbar">
   <a class="navbar-brand ml-5" href="#">
     <img src="<?= base_url('assets/images/logo_warna.png'); ?>" width="100" height="60" class="d-inline-block align-top rounded-circle" alt="">GLOBAL MEKAR MANDIRI</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
