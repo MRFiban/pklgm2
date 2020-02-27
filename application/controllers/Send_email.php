bf240766f5cd0bf9bab25f78fcbcba762f4e2e65
MRFiban <ridwaniban24@gmail.com>
2020-02-27 11:48:27 +0700

    login

--- application/controllers/Send_email.php
+++ application/controllers/Send_email.php
@@ -3,18 +3,38 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Send_email extends CI_Controller
 {
+
     public function index()
     {
-        // Konfigurasi email
-        $this->load->library('email');
+        $this->send_mail();
+    }
 
-        $this->email->from('your@example.com', 'Your Name');
-        $this->email->to('someone@example.com');
-        $this->email->cc('another@another-example.com');
-        $this->email->bcc('them@their-example.com');
 
-        $this->email->subject('Email Test');
-        $this->email->message('Testing the email class.');
+    public function send_mail()
+    {
+        $config = array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'smtp.gmail.com',
+            'smtp_port' => '465',
+            'smtp_user' => 'toniindriyanto27@gmail.com',
+            'smtp_pass' => '089605533669',
+            'smtp_crypto' => 'ssl',
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'wordwrap' => TRUE
+        );
 
-        $this->email->send();
+        $message = "hello word !";
+        $this->load->library('email', $config);
+        $this->email->set_newline("r\n");
+        $this->email->from('toniindriyanto27@gmail.com');
+        $this->email->to('toniindriyanto27@gmail.com');
+        $this->email->subject('subject : Send Mail');
+        $this->email->message($message);
+        if ($this->email->send()) {
+            echo 'Email sent.';
+        } else {
+            show_error($this->email->print_debugger());
+        }
+    }
 }
