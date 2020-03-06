77f2ef411cde410eb538ab5f6c10eefddd450df9
Jaykhalid <59681918+Jaykhalid@users.noreply.github.com>
2020-03-06 15:09:34 +0700

    perhalus

--- application/views/website/home_page.php
+++ application/views/website/home_page.php
@@ -52,35 +52,10 @@
         </u></h1>
       <h3> PT.GLOBAL MEKAR MANDIRI </h3>
     </div>
-    <br><br><br>
-    <!-- carosel -->
 
-    <!-- jumbotron -->
-    <div class="hayalasola">
-      <br><br>
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="card-group">
-            <div class="card-body">
-              <h5 class="card-title">KEUNGGULAN GLOBAL MEKAR MANDIRI</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ratione cupiditate nihil repellendus esse alias eligendi odit beatae, enim ipsa omnis vero eius, ex animi maiores molestias labore provident accusamus.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum a reiciendis, vero similique laborum repudiandae officia ducimus. Nemo nisi dolore ullam corrupti. Dolore fugit suscipit ducimus eum doloribus temporibus molestias?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nam reiciendis quos vel aliquid. Porro, dolor incidunt nesciunt perspiciatis odit, iure maxime architecto veritatis necessitatibus praesentium deleniti omnis inventore debitis.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui laborum, impedit alias dicta labore odio ratione enim ipsum, nulla nemo explicabo amet dolorem tempora. Similique impedit quia doloremque inventore perferendis.
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis quas est doloremque dolorem aspernatur omnis adipisci quod vel culpa ipsum doloribus enim at, suscipit temporibus tenetur totam et mollitia iusto!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel iusto laudantium magnam, quaerat ullam amet, deleniti laborum illo nisi labore dolorem distinctio! Voluptates doloribus libero, voluptas temporibus nemo ullam optio?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nisi, nemo corrupti ipsa quis reprehenderit ex id aut mollitia iure ut doloremque odio ipsam consequatur repellendus asperiores? Deleniti, quae velit!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum illo quaerat dignissimos dolores omnis inventore sunt voluptate voluptates? Illum magni voluptatum similique nemo deleniti officiis exercitationem molestias eius et rerum!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium, voluptatibus eaque eligendi assumenda quibusdam, error maxime officia quidem nobis enim alias illo, corporis et est quas id aliquid architecto.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est provident asperiores officia inventore voluptatem dicta deserunt corrupti sit aliquid minima accusamus veniam eveniet omnis atque, maxime facere beatae quos? Aliquid.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, sit dolorum distinctio suscipit non repellendus eaque at illum laudantium, voluptatum libero ut minima facere quia blanditiis officia atque nostrum aspernatur.</p>
-      </div>
-    </div>
-    <!-- jumbotron -->
+<br>
+<br>
+<br>
 
   <!-- Contact Section -->
   <section class="page-section" id="contact">
