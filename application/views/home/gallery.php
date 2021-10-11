ff53a15dcdd94464cf277f1d8a4641047192258e
Jaykhalid <jaydanekhalid@gmail.com>
2021-10-11 17:05:34 +0700

    perbaikan aplikasi mencakup perubahan isi konten copywriting, CSS, dan menonaktifkan penggunaan password dala integrasi ke database

--- application/views/home/gallery.php
+++ application/views/home/gallery.php
@@ -1,15 +1,14 @@
 <!-- Main Content -->
 <br><br>
-<h1 class="text-center"><font color="cyan"> Gallery Perusahaan </font></h1><hr class="divider mx-4">
+<h1 class="text-center"><font color="cyan"> Gallery Perusahaan </font></h1><hr class="divider">
 <br><br><br>
  <div class="row cols-4">
     <br><br><br>
                 <div class="card" style="width: 25rem; height: 10">
                     <img class="card-img-top" src="<?= base_url('assets/img/gallery.jpg'); ?>">
                         <div class="card-body">
-                            <h5 class="card-title text-center"><b><font color="cyan">Judul</b></font></h5><hr/>
-                            <p class="card-text"><font color="gold"> Ini deskripsi tolol jangan dibaca anak anjing bacod asu memek dsadas fdhfi asdjjaf adnsjkfeid sjdiasd adihfoidaf siadhoais djsad sajfasd dsandasj sakfnas dvsjf askfnkad faskfn
-                            </p>
+                            <h5 class="card-title text-center"><b><font color="orange">Bingkai Direksi GM2</b></font></h5><hr/>
+                            <p class="card-text"> Potret sesi foto bersama dengan para pendiri dan jajaran direksi PT. Global Mekar Mandiri</p>
                             <br><hr/>
                             <p calss="card-text" align="left"><small class="text-muted">Uploaded at <?= date('d F Y'); ?> </small></font></p>
                         </div>
@@ -18,8 +17,8 @@
                  <div class="card" style="width: 25rem;">
                     <img class="card-img-top" src="<?= base_url('assets/img/makan.jpg'); ?>"  alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title text-center"><b><font color="cyan">Judul</b></font></h5><hr/>
-                            <p class="card-text"><font color="gold">Foto ini merupakan dokumentasi pada saat Gathering Pembukaan Perusahaan sdkaslvjdnhilfjsaidnueobfdipabuo idhasohoas dahgihfod sfjdsfjds
+                            <h5 class="card-title text-center"><b><font color="orange">Breakfast Gathering</b></font></h5><hr/>
+                            <p class="card-text">Foto ini merupakan dokumentasi pada saat sesi makan bersama ketika Gathering Pembukaan Perusahaan.
                             </p>
                             <br><hr/>
                             <p calss="card-text" align="left"><small class="text-muted">Uploaded at <?= date('d F Y'); ?> </small></font></p>
@@ -29,8 +28,8 @@
                 <div class="card" style="width: 25rem;">
                     <img class="card-img-top" src="<?= base_url('assets/img/gallery3.jpg'); ?>"  alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title text-center"><b><font color="cyan">Judul</b></font></h5><hr/>
-                            <p class="card-text"><font color="gold">Foto ini merupakan dokumentasi pada saat Gathering Pembukaan Perusahaan sdkaslvjdnhilfjsaidnueobfdipabuo idhasohoas dahgihfod sfjdsfjds
+                            <h5 class="card-title text-center"><b><font color="orange">Completion of Legality</b></font></h5><hr/>
+                            <p class="card-text">Foto ini merupakan dokumentasi pada saat penuntasan penandatanganan berkas-berkas serta legalitas perusahaan yang ditangani langsung oleh Direktur Legal Hukum PT. GM2 yaitu pak Rendra Tjahjono S.H dalam acara Gathering Pembukaan Perusahaan.
                             </p>
                             <br><hr/>
                             <p calss="card-text" align="left"><small class="text-muted">Uploaded at <?= date('d F Y'); ?> </small></font></p>
@@ -45,8 +44,8 @@
                 <div class="card " align="center" style="width: 50rem;">
                     <img class="card-img-top" src="<?= base_url('assets/img/gallery4.jpg'); ?>">
                         <div class="card-body">
-                            <h5 class="card-title text-center"><b><font color="cyan">Teamwork</b></font></h5><hr/>
-                            <p class="card-text"><font color="gold">Kerjasama adalah Hal yang utama dalam sebuah Pergerakan Pembangunan maupun Perubahan.<br> Potret prosesi jabat tangan antara Direktur SDM PT. GM<sup>2</sup> dengan Chairman Rumah Makan Ampera
+                            <h5 class="card-title text-center"><b><font color="orange">Teamwork</b></font></h5><hr/>
+                            <p class="card-text">Kerjasama adalah Hal yang utama dalam sebuah Pergerakan Pembangunan maupun Perubahan.<br> Potret prosesi jabat tangan antara Direktur SDM PT. GM<sup>2</sup> dengan Chairman Rumah Makan Ampera
                             </p>
                             <br><hr/>
                             <p calss="card-text" align="left"><small class="text-muted">Uploaded at <?= date('d F Y'); ?> </small></font></p>
